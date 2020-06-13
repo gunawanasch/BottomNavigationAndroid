0639d200ea6a35d6d64dac6f70a7742a58c7d904
gunawanasch <gunawan.aschari@gmail.com>
2020-06-13 17:01:42 +0700

    change space main activity

--- app/src/main/java/com/gunawan/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/gunawan/bottomnavigation/MainActivity.kt
@@ -10,39 +10,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         title = resources.getString(R.string.home)
         loadFragment(HomeFragment())
 
         bottomNav.setOnNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.nav_home-> {
+                R.id.nav_home -> {
                     title = resources.getString(R.string.home)
                     loadFragment(HomeFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
 
-                R.id.nav_add-> {
+                R.id.nav_add -> {
                     title = resources.getString(R.string.add)
                     loadFragment(AddFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
 
-                R.id.nav_inbox-> {
+                R.id.nav_inbox -> {
                     title = resources.getString(R.string.inbox)
                     loadFragment(InboxFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
 
-                R.id.nav_more-> {
+                R.id.nav_more -> {
                     title = resources.getString(R.string.more)
                     loadFragment(MoreFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
-
             }
             false
-
         }
     }
 
