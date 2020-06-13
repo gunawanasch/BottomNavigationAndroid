2c497865f2f1e9cd456798d05e79251f99a490c8
gunawanasch <gunawan.aschari@gmail.com>
2020-06-13 16:56:01 +0700

    First commit

---
+++ app/src/main/java/com/gunawan/bottomnavigation/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.gunawan.bottomnavigation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvHome.text = "This is Home Fragment"
+    }
+
+}
---
+++ app/src/main/java/com/gunawan/bottomnavigation/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.gunawan.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        title = resources.getString(R.string.home)
+        loadFragment(HomeFragment())
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home-> {
+                    title = resources.getString(R.string.home)
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.nav_add-> {
+                    title = resources.getString(R.string.add)
+                    loadFragment(AddFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.nav_inbox-> {
+                    title = resources.getString(R.string.inbox)
+                    loadFragment(InboxFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.nav_more-> {
+                    title = resources.getString(R.string.more)
+                    loadFragment(MoreFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
+}
